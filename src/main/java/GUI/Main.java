658a329fb350f6239a00ddb4c9359556a0805834
prora <proraiaraffaele21@gmail.com>
2024-02-10 13:41:02 +0100

    commit

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -3,7 +3,7 @@ import Model.Utente;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[] start");
+        System.out.println("[ ] start");
 
         Utente u1 = new Utente("email1", "pass1", "Antonio", "Iodice", 'M');
         u1.print();
