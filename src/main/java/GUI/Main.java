5dc3fd082fc9f860c041e4af4a0369f97c563bd0
prora <proraiaraffaele21@gmail.com>
2024-02-10 12:55:58 +0100

    commit

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,11 +1,11 @@
 package main.java.GUI;
+import main.java.Model.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("caio");
+        System.out.println("[] start");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Utente u1 = new Utente();
+        u1.
     }
 }
---
+++ src/main/java/Model/Utente.java
@@ -0,0 +1,19 @@
+public class Utente {
+
+    private String Email;
+    private String Password;
+    private String Nome;
+    private String Cognome;
+    private char genere;
+
+    public Utente(String Email, String Password, String Nome, String Cognome, char genere){
+        this.Email=Email;
+        this.Password=Password;
+        this.Cognome=Cognome;
+        this.genere=genere;
+    }
+
+    public void print(){
+        System.printf("%s", Email);
+    }
+}
