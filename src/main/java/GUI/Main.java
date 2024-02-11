2eede5641162c6def36273a8541f3ddfd545bfe0
filix <florindozec@gmail.com>
2024-02-11 12:41:11 +0100

    porco

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -1,15 +1,14 @@
 package main.java.GUI;
 
+
 import main.java.Database.Connessione;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Main {
     private JPanel panel1;
-    private JButton accountButton;
-    private JTextField textField1;
-    private JLabel cercaLabel;
+    private JButton registerButton;
+    private JButton loginButton;
 
     public static void main(String[] args){
 
@@ -19,7 +18,7 @@ public class Main {
         frame.setContentPane(new Main().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        ImageIcon logo = new ImageIcon("C:\\Users\\prora\\Desktop\\ProgettoOO\\ProgettoOO\\src\\static\\logo4.png"); //aggiustare
+        ImageIcon logo = new ImageIcon("C:\\Users\\filix\\IdeaProjects\\ProgettoOO\\src\\static\\logo4.png"); //aggiustare
         frame.setIconImage(logo.getImage());
         frame.setSize(400, 300); // sempre dopo frame.pack()
         frame.setVisible(true);
