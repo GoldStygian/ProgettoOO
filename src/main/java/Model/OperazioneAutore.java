23ae9469dd7f296c9bbe5503defbe78ec2298d4e
prora <proraiaraffaele21@gmail.com>
2024-02-11 11:38:36 +0100

    commit

---
+++ src/main/java/Model/OperazioneAutore.java
@@ -0,0 +1,19 @@
+package main.java.Model;
+import java.util.Date;
+
+public class OperazioneAutore {
+
+    private Date Data;
+    private String Testo;
+    private boolean Link;
+    private Integer LinkPagina;
+
+    public OperazioneAutore(Date Data, String Testo, boolean Link, Integer LinkPagina){
+        this.Data=Data;
+        this.Testo=Testo;
+        this.Link=Link;
+        this.LinkPagina=LinkPagina;
+    }
+
+
+}
