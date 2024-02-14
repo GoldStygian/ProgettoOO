a769a9b7013ecff711ed893548d973765df9076f
prora <proraiaraffaele21@gmail.com>
2024-02-14 23:00:28 +0100

    commit

--- src/main/java/DAO/GetWikiDAO.java
+++ src/main/java/DAO/GetWikiDAO.java
@@ -1,10 +1,12 @@
 package main.java.DAO;
 
+import main.java.Model.Frase;
+
 import java.sql.SQLException;
 import java.util.HashMap;
 
 public interface GetWikiDAO {
 
-    public HashMap<Integer, String> getWikiPage(String titolo) throws SQLException;
+    public HashMap<Integer, Frase> getWikiPage(String titolo) throws SQLException;
 
 }
--- src/main/java/ImplementazionePostgresDAO/GetWikiDAO.java
+++ src/main/java/ImplementazionePostgresDAO/GetWikiDAO.java
@@ -1,5 +1,8 @@
 package main.java.ImplementazionePostgresDAO;
 
+import main.java.Model.Frase;
+import main.java.Model.Link;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,19 +11,26 @@ import java.util.HashMap;
 
 public class GetWikiDAO implements main.java.DAO.GetWikiDAO {
 
-    public HashMap<Integer, String> getWikiPage(String titolo) throws SQLException {
+    public HashMap<Integer, Frase> getWikiPage(String titolo) throws SQLException {
         Connection con = new ConnessionePostgesDAO().openConnection();
         Statement statement = con.createStatement();
 
-        String query="SELECT posizionem testo, link FROM frase WHERE pagina IN (SELECT id_pagina FROM pagina WHERE titolo = '%s')".formatted(titolo);
+        String query="SELECT posizione, testo, link FROM frase WHERE pagina IN (SELECT id_pagina FROM pagina WHERE titolo = '%s')".formatted(titolo);
         ResultSet WikiPage = statement.executeQuery(query);
 
-        HashMap<Integer, String> Frasi = new HashMap<Integer, String>(); // ci sta sempre un elemento nella pagina
+        HashMap<Integer, Frase> Frasi = new HashMap<Integer, Frase>(); // ci sta sempre un elemento nella pagina
+        Integer index = 0;
         while(WikiPage.next()){
-
-            //Frasi.put("USA", "Washington DC");
-            System.out.println(WikiPage.getString("testo"));
+            Frase frase;
+            index+=1;
+            if (WikiPage.getBoolean("link")){
+                frase = new Link(WikiPage.getString("testo"), WikiPage.getInt("posizione"));
+            }else{
+                frase = new Frase(WikiPage.getString("testo"), WikiPage.getInt("posizione"));
+            }
+            Frasi.put(index, frase);
         }
+        System.out.println(Frasi);
 
         WikiPage.close();
         statement.close();
