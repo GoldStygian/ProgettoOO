3936d32c2399e490ca3ecb98b868c709aaf31b66
filix <florindozec@gmail.com>
2024-02-14 23:02:32 +0100

    porco dio

--- src/main/java/DAO/RegisterDAO.java
+++ src/main/java/DAO/RegisterDAO.java
@@ -1,6 +1,9 @@
 package main.java.DAO;
 
+import java.sql.SQLException;
+
 public interface RegisterDAO {
 
+    public String RegisterUser(String Nome, String Cognome, String Genere, String Email, String Password) throws SQLException;
 
 }
