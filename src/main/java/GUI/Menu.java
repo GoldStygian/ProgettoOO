e763ccf9235f5554080df7a6dc9772caae27d0ae
filix <florindozec@gmail.com>
2024-02-14 20:12:44 +0100

    porco dio

--- src/main/java/GUI/Menu.java
+++ src/main/java/GUI/Menu.java
@@ -13,12 +13,13 @@ public class Menu extends JMenuBar {
 
     public Menu(MainJFrame frame, JMenu menu){
 
+        GuiPresetComponet t = new GuiPresetComponet(frame);
 
         menu = new JMenu("Menu");
         menu.setFont(frame.getFontToolBar());
         menu.setBackground(frame.getColorToolBar());
 
-        JMenuItem menuItem = new MenuItem(frame,"ciao", null);
+        JMenuItem menuItem = new MenuItem(frame,"Impostazioni", new ImageIcon(t.ResizeIcon(20, 20, new ImageIcon("src\\main\\resources\\gear.png"))));
         menu.add(menuItem);
         menuItem = new MenuItem(frame,"ciao2", null);
         menu.add(menuItem);
