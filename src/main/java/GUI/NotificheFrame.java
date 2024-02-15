9a305d0dc473b64cb0c468973cc12be626f2c8d9
filix <florindozec@gmail.com>
2024-02-16 00:16:04 +0100

    porco dio

--- src/main/java/GUI/NotificheFrame.java
+++ src/main/java/GUI/NotificheFrame.java
@@ -23,46 +23,45 @@ public class NotificheFrame extends JFrame {
         this.setResizable(false);
         //this.setUndecorated(true);
         this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        MainJPanel.setBackground(frame.getColorBack());
 
 
-       /* GridBagConstraints gbc;
+        /*
+       GridBagConstraints gbc;
         gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        //gbc.anchor = GridBagConstraints.NORTH;
+        gbc.anchor = GridBagConstraints.NORTH;
         gbc.gridy = GridBagConstraints.RELATIVE;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        MainJPanel.add(new NotifichePanel(),gbc);
-      gbc.gridx = 0;
+        MainJPanel.add(new NotifichePanel(frame),gbc);
+        gbc.gridx = 0;
         gbc.gridy = 1;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 2;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 3;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 4;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 5;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 6;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 7;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 8;
-        MainJPanel.add(new NotifichePanel(),gbc);
+        MainJPanel.add(new NotifichePanel(frame),gbc);
         */
 
-
-
     }
-
 
 
 }
--- src/main/java/GUI/NotifichePanel.java
+++ src/main/java/GUI/NotifichePanel.java
@@ -7,8 +7,9 @@ public class NotifichePanel extends JPanel {
 
     private JPanel MainPanel;
 
-    public NotifichePanel() {
+    public NotifichePanel(MainJFrame frame) {
         this.add(MainPanel);
+        this.setBackground(frame.getColorBack());
     }
 
 
