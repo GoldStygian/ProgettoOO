4adb1a61537eb3531cf4c407165a99486d400368
filix <florindozec@gmail.com>
2024-02-15 23:47:30 +0100

    porco dio

--- src/main/java/GUI/NotificheFrame.java
+++ src/main/java/GUI/NotificheFrame.java
@@ -25,10 +25,44 @@ public class NotificheFrame extends JFrame {
         this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
 
 
+       /* GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        //gbc.anchor = GridBagConstraints.NORTH;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        MainJPanel.add(new NotifichePanel(),gbc);
+      gbc.gridx = 0;
+        gbc.gridy = 1;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        MainJPanel.add(new NotifichePanel(),gbc);
+        */
+
+
+
     }
 
 
-    private void createUIComponents() {
 
-    }
 }
---
+++ src/main/java/GUI/NotifichePanel.java
@@ -0,0 +1,16 @@
+package main.java.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NotifichePanel extends JPanel {
+
+    private JPanel MainPanel;
+
+    public NotifichePanel() {
+        this.add(MainPanel);
+    }
+
+
+}
+
