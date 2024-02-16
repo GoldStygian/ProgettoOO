e1454e77cc753f40063d4ee200106c337335aeda
filix <florindozec@gmail.com>
2024-02-16 20:50:27 +0100

    porco dio

--- src/main/java/GUI/NotificheFrame.java
+++ src/main/java/GUI/NotificheFrame.java
@@ -9,6 +9,8 @@ import java.util.MissingFormatArgumentException;
 public class NotificheFrame extends JFrame {
 
     private JPanel MainJPanel;
+    private JPanel Notifiche;
+    private JPanel Divisore;
 
 
     public NotificheFrame(String Nome, MainJFrame frame, Controller controller) {
@@ -25,16 +27,16 @@ public class NotificheFrame extends JFrame {
         this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
         MainJPanel.setBackground(frame.getColorBack());
 
-
+        Notifiche.add(new NotifichePanel(frame));
         /*
-       GridBagConstraints gbc;
+        GridBagConstraints gbc;
         gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.NORTH;
         gbc.gridy = GridBagConstraints.RELATIVE;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        MainJPanel.add(new NotifichePanel(frame),gbc);
+        Notifiche.add(new NotifichePanel(frame),gbc);
         gbc.gridx = 0;
         gbc.gridy = 1;
         MainJPanel.add(new NotifichePanel(frame),gbc);
